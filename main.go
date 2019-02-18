61cbb51e177b28ae1daf6d2f09752edd949f99a7
Daniyarkadyrbekov <d.kadyrbekov@mail.ru>
2019-02-18 23:36:05 +0300

    beggining

--- main.go
+++ main.go
@@ -1,17 +1,39 @@
 package main
 
-const N = 10
+import (
+	"fmt"
+	"math/rand"
+)
+
+const N = 5
 
 type Point struct{
 	Weight int
 	Final bool
 }
 
-
+var Points [N]Point
 var matrix [N][N]int
+var way []int
 
 func main() {
 	initializeMatrix()
+
+	from := 0
+	to := 5
+	way = countWay(from, to)
+
+	showResults()
+}
+
+func countWay(from, to int) []int {
+	way := make([]int, 1)
+
+	for i := 0; i < N; i++ {
+
+	}
+
+	return way
 }
 
 func initializeMatrix() {
@@ -21,9 +43,18 @@ func initializeMatrix() {
 				matrix[i][j] = 0
 				continue
 			}
-			randNumber := 0
+			randNumber := rand.Int() % 10
 			matrix[j][i] = randNumber
 			matrix[i][j] = randNumber
 		}
+		Points[i].Final = false
+		Points[i].Weight = 100000
 	}
+}
+
+func showResults() {
+	for i := 0; i < N; i++ {
+		fmt.Println(matrix[i])
+	}
+	fmt.Println(Points)
 }
